cb0af24e7cebfe9bc764cb9dfd6d304a37a1b304
ho28596 <fabio.borghetto@bancobpm.it>
2026-03-20 08:55:17 +0100

    Update items.component.js

--- src/items.component.js
+++ src/items.component.js
@@ -3,7 +3,7 @@
   
   angular.module("MenuApp")
     .component("items", {
-      templateUrl: "src/tempalte/items.template.html",
+      templateUrl: "src/template/items.template.html",
       controller: ItemsComponentController,
       bindings: {
         list: "<"
